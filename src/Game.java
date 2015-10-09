6ee944c872533ec10c0c9969efbd14a4340f5ca6
MattBrown <litteriver1337@gmail.com>
2015-10-09 10:50:36 -0400

    How to organize code into Player Class

--- src/Game.java
+++ src/Game.java
@@ -8,34 +8,10 @@ import java.util.Scanner;
 public class Game {
     public static void run() throws Exception {
         System.out.println("Welcome to my text adventure");
-        System.out.println("What is your name, traveler?");
 
-        Scanner scanner = new Scanner(System.in);
-        String name = scanner.nextLine();
-        System.out.println("Good luck, " + name);
-
-        System.out.println("[1] Pick up a sword");
-        System.out.println("[2] Pick up mallet");
-        String weapon = scanner.nextLine();
-        int weaponNum = Integer.valueOf(weapon);
-        if (weaponNum == 1){
-            System.out.println("That's fine sword!");
-        } else if (weaponNum == 2){
-            System.out.println("That's a heavy mallet");
-        } else {
-            throw new Exception("Invalid Weapon.");
-        }
-
-        System.out.println("[1] Enter the forest");
-        System.out.println("[2] Enter the undeground tunnel");
-        String area = scanner.nextLine();
-        int areaNum = Integer.valueOf(area);
-        if (areaNum == 1){
-            System.out.println("Entering the forest....");
-        } else if (areaNum == 2){
-            System.out.println("Enterting the tunnel...");
-        } else {
-            throw new Exception("Invalid area.");
-        }
+      Player player = new Player();
+        player.chooseName();
+        player.chooseWeapon();
+        player.chooseArea();
     }
 }
---
+++ src/Player.java
@@ -0,0 +1,47 @@
+import java.util.Scanner;
+
+/**
+ * Created by MattBrown on 10/9/15.
+ */
+public class Player {
+    String name;
+    String weapon;
+    String area;
+
+    void chooseName() {
+        System.out.println("What is your name, traveler?");
+        Scanner scanner = new Scanner(System.in);
+        name = scanner.nextLine();
+        System.out.println("Good luck, " + name);
+    }
+
+    void chooseWeapon() throws Exception {
+        System.out.println("[1] Pick up a sword");
+        System.out.println("[2] Pick up mallet");
+        Scanner scanner = new Scanner(System.in);
+        weapon = scanner.nextLine();
+        int weaponNum = Integer.valueOf(weapon);
+        if (weaponNum == 1){
+            System.out.println("That's fine sword!");
+        } else if (weaponNum == 2){
+            System.out.println("That's a heavy mallet");
+        } else {
+            throw new Exception("Invalid Weapon.");
+        }
+    }
+
+    void chooseArea() throws Exception {
+        System.out.println("[1] Enter the forest");
+        System.out.println("[2] Enter the undeground tunnel");
+        Scanner scanner = new Scanner(System.in);
+        area = scanner.nextLine();
+        int areaNum = Integer.valueOf(area);
+        if (areaNum == 1){
+            System.out.println("Entering the forest....");
+        } else if (areaNum == 2){
+            System.out.println("Enterting the tunnel...");
+        } else {
+            throw new Exception("Invalid area.");
+        }
+    }
+}
