9d4beb3dd64046c57c509383b6b91b912e953ade
MattBrown <litteriver1337@gmail.com>
2015-10-09 11:19:03 -0400

    /help and /exit and Loops

--- src/Game.java
+++ src/Game.java
@@ -1,5 +1,6 @@
 import com.sun.tools.doclets.formats.html.SourceToHTMLConverter;
 
+import java.io.InputStream;
 import java.util.Scanner;
 
 /**
@@ -9,9 +10,27 @@ public class Game {
     public static void run() throws Exception {
         System.out.println("Welcome to my text adventure");
 
-      Player player = new Player();
-        player.chooseName();
-        player.chooseWeapon();
-        player.chooseArea();
+      while (true) {
+          Player player = new Player();
+          player.chooseName();
+          player.chooseWeapon();
+          player.chooseArea();
+      }
+    }
+
+    static String nextLine(){
+        Scanner scanner = new Scanner(System.in);
+        String s = scanner.nextLine();
+        if (s.startsWith("/")){
+            if (s.equals("/help")){
+                System.out.println("These are the available commands:");
+                System.out.println("/help => List available commands");
+            }else if (s.equals("/exit")){
+                System.exit(0);
+            }
+            return nextLine();
+        }else {
+            return s;
+        }
     }
 }
